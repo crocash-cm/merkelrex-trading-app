2b063ac8d0d359222b96a85bbd80ed4520d057be
crocash-cm <jashford.work@gmail.com>
2025-06-01 19:14:14 +0100

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "OrderBookEntry.h"
+#include "MerkelMain.h"
+#include "csvReader.h"
+
+int main() 
+{
+    // Start program
+    MerkelMain app{};
+    app.init();
+    return 0;
+}

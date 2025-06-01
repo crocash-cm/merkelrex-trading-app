fe45cdb1d912ff3889418c0a9444ba23b43d0199
crocash-cm <jashford.work@gmail.com>
2025-06-01 19:07:22 +0100

    Add MerkelMain.h

---
+++ MerkelMain.h
@@ -0,0 +1,37 @@
+#ifndef MERKELMAIN_H
+#define MERKELMAIN_H
+// Above is include guard, another option is #pragma once
+#include "OrderBookEntry.h"
+#include "OrderBook.h"
+
+class MerkelMain 
+{
+    public:
+        // Constructor and initializer
+        MerkelMain();
+        /** Call this to start sim */
+        void init();
+
+    private:
+        // Methods
+        void loadOrderBook();
+        void printMenu();
+        int getUserOption();
+        void processUserOption(int userOption);
+        void printHelp();
+        void printStats();
+        void placeAsk();
+        void placeBid();
+        void printWallet();
+        void gotoNextTimeUnit();
+        void printError();
+
+        // Fields
+        bool isRunning;
+        std::string currentTime;
+        // Order book
+        OrderBook orderBook{"OrderBookDB.csv"};
+        
+};
+// Endif for include guard
+#endif

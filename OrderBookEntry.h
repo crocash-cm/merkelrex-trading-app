8a730f612c6ce92ed7b16bee35ddb16edf339d08
crocash-cm <jashford.work@gmail.com>
2025-06-01 20:45:06 +0100

    Add compareFunctions.

--- OrderBookEntry.h
+++ OrderBookEntry.h
@@ -20,6 +20,14 @@ class OrderBookEntry
         static OrderBookType stringToOBT(std::string s);
         /** Returns true if e1 timestamp smaller than e2 */
         static bool compareByTimestamp(OrderBookEntry& e1, OrderBookEntry& e2);
+        static bool compareByPriceAsc(OrderBookEntry& e1, OrderBookEntry& e2)
+        {
+            return e1.price < e2.price;
+        }
+        static bool compareByPriceDesc(OrderBookEntry& e1, OrderBookEntry& e2)
+        {
+            return e1.price > e2.price;
+        }
 
         // Fields
         std::string timestamp;

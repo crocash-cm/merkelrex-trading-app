fae8428f93df202e91ca2080bc532e8e6eca22b0
crocash-cm <jashford.work@gmail.com>
2025-06-01 19:12:23 +0100

    Add OrderBookEntry.cpp

---
+++ OrderBookEntry.cpp
@@ -0,0 +1,42 @@
+#include <stdexcept>
+#include "OrderBookEntry.h"
+
+OrderBookEntry::OrderBookEntry(std::string _timestamp, std::string _product,  OrderBookType _type, double _price, double _amount)
+: timestamp(_timestamp),
+    product(_product),
+    type(_type),
+    price(_price),
+    amount(_amount)
+{
+      
+}
+
+std::string OrderBookEntry::obtToString()
+{
+    OrderBookType t = this->type;
+    switch (t)
+    {
+    case OrderBookType::ask:
+        return "ask";
+    case OrderBookType::bid:
+        return "bid";
+    default:
+        throw std::invalid_argument("OrderBookEntry::obtToString invalid type.");
+    }
+}
+
+ OrderBookType OrderBookEntry::stringToOBT(std::string s)
+ {
+    if (s == "ask")
+    {
+        return OrderBookType::ask;
+    }
+    else if (s == "bid")
+    {
+        return OrderBookType::bid;
+    }
+    else
+    {
+        throw std::invalid_argument("OrderBookEntry::stringToOBT invalid argument");
+    }
+ }

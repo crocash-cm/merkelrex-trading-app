7372577526a65e9ad1b2f6868e5477b08dd15fb5
crocash-cm <jashford.work@gmail.com>
2025-06-02 14:19:18 +0100

    Wrote Wallet.cpp

---
+++ Wallet.cpp
@@ -0,0 +1,25 @@
+ #include <string>
+ #include <map>
+ #include "Wallet.h"
+ 
+ Wallet();
+ {
+
+ }
+
+ void Wallet::depositCurrency(std::string type, double amount)
+ {
+
+ }
+
+ /** Check whether wallet contains enough of currency */
+bool Wallet::containsCurrency(std::string type, double amount)
+{
+    return false;
+}
+
+/** Generate string representation of the wallet */
+std::string Wallet::toString()
+{
+    return "test";
+}
